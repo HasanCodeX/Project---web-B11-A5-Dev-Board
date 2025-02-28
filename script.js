4bca35e226d847559b3a2a208d0ffc207dd7dd48
hasancodex <dev.hasan10@gmail.com>
2025-03-01 03:03:56 +0600

    add present time

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Function for discover button and blog page redirection.....
+
 document.getElementById("discoverBtn").addEventListener("click", function () {
-    window.location.href = "blog.html";
+  window.location.href = "blog.html";
+});
+
+// Function for show present date ....
+document.addEventListener("DOMContentLoaded", function () {
+  const dateElement = document.getElementById("currentDate");
+
+  const today = new Date();
+  const options = { year: "numeric", month: "long", day: "numeric" };
+
+  dateElement.textContent = today.toLocaleDateString("en-US", options);
+});
+
+// Function for show present day ...
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dateElement = document.getElementById("currentDay");
+
+  const today = new Date();
+  const options = { weekday: "long" };
+
+  dateElement.textContent = today.toLocaleDateString("en-US", options);
 });
